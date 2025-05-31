be81e0b81bf03b650cc751a4c5f089d7c35f0601
DoHuy <huydqph50651@gmail.com>
2025-05-31 21:21:18 +0700

    [huydq] remove test route

--- routes/admin/admin.php
+++ routes/admin/admin.php
@@ -1,11 +1,3 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-
-Route::get('/test', function () {
-    return response()->json([
-        'status' => 'true',
-        'data' => 'Kết nối thành công'
-    ]);
-});
