3072a17286a7924eeead12dce0f28d826451f4fe
thaian45 <thaian040504@gmail.com>
2025-06-10 10:53:41 +0700

    xóa bỏ khu vực bàn và quản lý bàn theo chi nhánh

--- routes/admin/admin.php
+++ routes/admin/admin.php
@@ -11,27 +11,14 @@ Route::prefix('admin')->group(function () {
     // # branchs
     Route::get('customers/list', [CustomerController::class, 'getListCustomers']);
     Route::get('customers/{id}/detail', [CustomerController::class, 'getCustomerDetail']);
-   
+
     Route::post('customers/{id}/update', [CustomerController::class, 'updateCustomer']);
     Route::get('customers/trash', [CustomerController::class, 'listTrashedCustomer']);
     Route::delete('customers/{id}/soft/delete', [CustomerController::class, 'softDeleteCustomer']);
     Route::delete('customers/{id}/force/delete', [CustomerController::class, 'forceDeleteCustomer']);
     Route::post('customers/{id}/restore', [CustomerController::class, 'restoreCustomer']);
 
-    // # table areas templates
-    Route::get('table-areas-templates/list', [TableAreaTemplateController::class, 'getListTableAreasTemplate']);
-    Route::get('table-areas-templates/{slug}/detail', [TableAreaTemplateController::class, 'getTableAreaTemplateDetail']);
-    Route::post('table-areas-templates/create', [TableAreaTemplateController::class, 'createTableAreaTemplate']);
-    Route::post('table-areas-templates/{slug}/update', [TableAreaTemplateController::class, 'updateTableAreaTemplate']);
-    Route::delete('table-areas-templates/{slug}/delete', [TableAreaTemplateController::class, 'deleteTableAreaTemplate']);
 
-    // # table areas
-    Route::get('table-areas/list/{branchId}', [TableAreaController::class, 'getTableAreasByBranch']);
-    Route::post('table-areas/create/{branchId}', [TableAreaController::class, 'createTableAreaForBranch']);
-    Route::post('table-areas/{slug}/update/{branchId}', [TableAreaController::class, 'updateTableAreaForBranch']);
-    Route::post('table-areas/create-for-all-branches', [TableAreaController::class, 'createTableAreaForAllBranches']);
-    Route::get('table-areas/{slug}/detail', [TableAreaController::class, 'getTableAreaDetail']);
-    Route::delete('table-areas/{slug}/delete/{branchId}', [TableAreaController::class, 'deleteTableAreaForBranch']);
     ## categories
     Route::get('categories/list', [CategoryController::class, 'getListCategories']);
     Route::get('categories/{slug}/detail', [CategoryController::class, 'getCategoryDetail']);
