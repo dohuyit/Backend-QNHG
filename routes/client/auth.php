b136fca1383f1f01a293b27fed268c7ee64a9bd1
DoHuy <huydqph50651@gmail.com>
2025-06-25 21:46:04 +0700

    comment api client

--- routes/client/auth.php
+++ routes/client/auth.php
@@ -3,6 +3,12 @@
 use App\Http\Controllers\Client\Auth\AuthController;
 use Illuminate\Support\Facades\Route;
 
+
+// http://127.0.0.1:8000/api/register
 Route::post('/register', [AuthController::class, 'register']);
+
+// http://127.0.0.1:8000/api/login
 Route::post('/login', [AuthController::class, 'login']);
+
+// http://127.0.0.1:8000/api/logout
 Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
